54ff9a1d255263b046705ee08910a22f8f1cc0d5
rarhoade <rahul.rhoades.dev@gmail.com>
2025-04-19 16:27:32 -0600

    Adding static file server to http flow

--- hello.go
+++ hello.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt";
-	"net/http";
+	"fmt"
+	"net/http"
 )
 
 func main() {
+	f := http.FileServer(http.Dir("static/"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
+	http.Handle("/static/", http.StripPrefix("/static/", f))
+
 	http.ListenAndServe(":80", nil)
 }
